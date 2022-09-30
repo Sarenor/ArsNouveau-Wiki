e06367c7eb3a5affbcade40d58ee3a194f29bf30
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-30 18:40:37 +0200

    Corrected url again

--- src/lib/utils/apiUtils.ts
+++ src/lib/utils/apiUtils.ts
@@ -4,6 +4,6 @@ export const apiBaseURL = () => {
 	if (dev) {
 		return 'http://localhost:30080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
 	} else {
-		return 'https://cors-anywhere-sshmngscpq-uc.a.run.app:8080/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
+		return 'https://cors-anywhere-sshmngscpq-uc.a.run.app/https://api.github.com/repos/baileyholl/Ars-Nouveau/zipball';
 	}
 };
