ea3be4c67c80b03b9473afc854901a1f44da5663
Dominik Röschke <dominik.roeschke@web.de>
2024-05-25 11:19:42 +0200

    Updated with supporter starby names & hover bio

--- src/lib/components/header/StarbuncleRun.svelte
+++ src/lib/components/header/StarbuncleRun.svelte
@@ -1,38 +1,68 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import anime from 'animejs';
+	import { popup, type PopupSettings } from '@skeletonlabs/skeleton';
 
-	let state = "none";
-	let src = "/runningbuncle/starbuncle_run_purple_border.gif";
+	let state = 'none';
 	let changeBegan = 0;
+	let starbuncles = [
+		{
+			name: 'Bailey',
+			adopter: 'Ars Nouveau Team',
+			bio: 'Ars Nouveau is a passion project brought to life by hundreds of contributions from the community. We hope you enjoy this Rainbow-buncle as much as we enjoy making this mod! Thanks for playing!"',
+			color: 'red'
+		}
+	];
+	let starbuncleIndex = 0;
+	let src = '/runningbuncle/starbuncle_run_purple_border.gif';
+	let popupSettings: PopupSettings = {
+		event: 'hover',
+		placement: 'bottom',
+		target: 'bio-popup'
+	};
+
+	function getRandomStarbuncleIndex() {
+		return Math.floor(Math.random() * starbuncles.length);
+	}
 
 	onMount(() => {
-		anime({
-			targets: '.buncle-box',
-			translateX: ["-25%", "80%"],
-			delay: 1000,
-			endDelay: 1000,
-			loop: true,
-			duration: 3500,
-			direction: 'alternate',
-			easing: 'linear',
-			changeBegin: function(anim) {
-				changeBegan++;
-				if(changeBegan % 2 === 1) {
-					state = "FORWARDS";
-					src = "/runningbuncle/starbuncle_run_purple_border.gif";
-				} else {
-					state = "BACKWARDS";
-					src = "/runningbuncle/starbuncle_run_purple_border.gif";
+		if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+			anime({
+				targets: '.buncle-box',
+				translateX: ['-25%', '79%'],
+				delay: 1000,
+				endDelay: 1000,
+				loop: true,
+				duration: 3500,
+				direction: 'alternate',
+				easing: 'linear',
+				changeBegin: function(anim) {
+					changeBegan++;
+					if (changeBegan % 2 === 1) {
+						state = 'FORWARDS';
+						src = '/runningbuncle/starbuncle_run_purple_border.gif';
+					} else {
+						state = 'BACKWARDS';
+						src = '/runningbuncle/starbuncle_run_purple_border.gif';
+					}
+				},
+				changeComplete: function(anim) {
+					if (changeBegan % 2 === 1) {
+						state = 'WAITING';
+						src = '/runningbuncle/lilly.png';
+					} else {
+						starbuncleIndex = getRandomStarbuncleIndex();
+					}
 				}
-			},
-			changeComplete: function(anim) {
-				if(changeBegan % 2 === 1) {
-					state = "WAITING";
-					src = "/runningbuncle/lilly.png";
-				}
-			}
-		});
+			});
+		}
+
+		fetch('https://raw.githubusercontent.com/baileyholl/Ars-Nouveau/main/supporters.json')
+			.then(response => response.json())
+			.then(data => {
+				starbuncles = data.starbuncleAdoptions;
+				starbuncleIndex = getRandomStarbuncleIndex();
+			});
 	});
 </script>
 
@@ -51,7 +81,7 @@
 
     .buncle {
         height: 50px;
-				margin-left: -3px;
+        margin-left: -3px;
     }
 
     .buncle-name {
@@ -61,16 +91,24 @@
         transform: translate(-50%, -50%);
     }
 
-		.mirrored {
+    .mirrored {
         transform: scaleX(-1);
-				margin-left: -7px;
-		}
+        margin-left: -7px;
+    }
 </style>
 
 
-<div class="buncle-container">
-	<p class="buncle-name">Bailey</p>
-	<div class="buncle-box bg-surface-100-800-token w-full">
-		<img {src} alt="animated running Starbuncle" class="buncle" class:mirrored={state === "BACKWARDS"}/>
+<div class="buncle-container" use:popup={popupSettings}>
+	<div class="buncle-name flex flex-col items-center">
+		<p>{starbuncles[starbuncleIndex].name}</p>
+		<p class="text-xs text-gray-500">{starbuncles[starbuncleIndex].adopter}</p>
 	</div>
+	<div class="buncle-box bg-surface-100-800-token w-full">
+		<img {src} alt="animated running Starbuncle" class="buncle" class:mirrored={state === "BACKWARDS"} />
+	</div>
+</div>
+
+<div data-popup="bio-popup" class="card variant-glass-primary p-4 max-w-lg">
+	{starbuncles[starbuncleIndex].bio}
+	<div class="arrow variant-filled-primary" />
 </div>
